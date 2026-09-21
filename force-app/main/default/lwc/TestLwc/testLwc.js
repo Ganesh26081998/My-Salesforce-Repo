99cc2a99995bcb61c067a20902a15ce08375071d
Ganesh <ganesh.uppin28@gmail.com>
2026-09-21 12:22:48 +0530

    Test code pushed

--- force-app/main/default/lwc/TestLwc/testLwc.js
+++ force-app/main/default/lwc/TestLwc/testLwc.js
@@ -3,5 +3,6 @@ import { LightningElement } from 'lwc';
 export default class TestLwc extends LightningElement {
     connectedCallback() {
         console.log('testLwc component loaded');
+        console.log('testLwc component loaded');
     }
 }
