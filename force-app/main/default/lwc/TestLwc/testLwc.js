f84daf773b9ab15f08e7b7e5538497d180c598c7
Ganesh <ganesh.uppin28@gmail.com>
2026-09-22 10:41:58 +0530

    adding new line

--- force-app/main/default/lwc/TestLwc/testLwc.js
+++ force-app/main/default/lwc/TestLwc/testLwc.js
@@ -5,5 +5,6 @@ export default class TestLwc extends LightningElement {
         console.log('testLwc component loaded');
         console.log('testLwc component loaded');
         console.log('testLwc new component loaded');
+        console.log('testLwc new branch component loaded');
     }
 }
